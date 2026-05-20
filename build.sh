f116a8869001b0e1d7a1341401978cdc020491f8
Oleksii Moskalenko <alfacentavra.a@gmail.com>
2026-05-20 12:23:04 +0300

    fixed docker

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build "$@" --platform linux/arm64 -t rpi64-builder .
-docker run --rm --platform linux/arm64 -v "$(pwd):/workspace" -w /workspace rpi64-builder bash -c "mkdir -p /build && cp -r . /build/src && cd /build/src && dpkg-buildpackage -us -uc -b && cp ../*.deb /workspace/"
+docker build --progress=plain "$@" --platform linux/arm64 -t rpi64-builder .
+docker run --rm --platform linux/arm64 -v "$(pwd):/workspace" -w /workspace rpi64-builder bash -c "mkdir -p /build && cp -r . /build/src && cd /build/src && DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -us -uc -b && cp ../*.deb /workspace/"
