9cbc55ec2d2c65a8a1b3b54b5d9c9805ea98d034
Oleksii Moskalenko <alfacentavra.a@gmail.com>
2026-06-03 01:17:45 +0300

    reformat

--- build.sh
+++
@@ -1,2 +0,0 @@
-docker build --progress=plain "$@" --platform linux/arm64 -t rpi64-builder .
-docker run --rm --platform linux/arm64 -v "$(pwd):/workspace" -w /workspace rpi64-builder bash -c "mkdir -p /build && cp -r . /build/src && cd /build/src && DEB_BUILD_OPTIONS=noautodbgsym dpkg-buildpackage -us -uc -b && cp ../*.deb /workspace/"
